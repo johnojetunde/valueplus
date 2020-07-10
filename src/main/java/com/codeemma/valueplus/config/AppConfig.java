913eb405412c898e8a3b9cf1e668621f6fce009c
Emmanuel Olayinka <emmanuel.olayinka@ruptela.com>
2020-07-10 12:46:17 +0300

    fix mvc config

--- src/main/java/com/codeemma/valueplus/config/AppConfig.java
+++ src/main/java/com/codeemma/valueplus/config/AppConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
 public class AppConfig {
 
     @Bean
--- src/main/java/com/codeemma/valueplus/swagger/SwaggerConfiguration.java
+++ src/main/java/com/codeemma/valueplus/swagger/SwaggerConfiguration.java
@@ -3,6 +3,7 @@ package com.codeemma.valueplus.swagger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
