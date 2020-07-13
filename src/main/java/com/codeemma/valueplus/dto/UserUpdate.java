8b56c06c13f6a82d205289c87bd89a67d3488a22
Emmanuel Olayinka <emmanuel.olayinka@ruptela.com>
2020-07-13 19:20:31 +0300

    add ombock anontations to UserUpdate

--- src/main/java/com/codeemma/valueplus/dto/UserUpdate.java
+++ src/main/java/com/codeemma/valueplus/dto/UserUpdate.java
@@ -1,10 +1,18 @@
 package com.codeemma.valueplus.dto;
 
 import com.codeemma.valueplus.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
 public class UserUpdate {
     @NotEmpty
     private String firstname;
