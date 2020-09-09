d57fa30056ddde4a843150f8c161bee806e99107
John Ojetunde <john.ojetunde@telesoftas.com>
2020-09-10 02:33:48 +0300

    feat: added transaction status for admin

--- src/main/java/com/codeemma/valueplus/domain/service/abstracts/TransferService.java
+++ src/main/java/com/codeemma/valueplus/domain/service/abstracts/TransferService.java
@@ -2,7 +2,7 @@ package com.codeemma.valueplus.domain.service.abstracts;
 
 import com.codeemma.valueplus.app.exception.ValuePlusException;
 import com.codeemma.valueplus.app.model.PaymentRequestModel;
-import com.codeemma.valueplus.domain.enums.TransactionStatus;
+import com.codeemma.valueplus.domain.enums.TransactionStatusFilter;
 import com.codeemma.valueplus.domain.model.TransactionModel;
 import com.codeemma.valueplus.persistence.entity.User;
 import org.springframework.data.domain.Page;
@@ -31,7 +31,7 @@ public interface TransferService {
                                                  Pageable pageable) throws ValuePlusException;
 
     Page<TransactionModel> filter(User user,
-                                  TransactionStatus status,
+                                  TransactionStatusFilter status,
                                   LocalDate startDate,
                                   LocalDate endDate,
                                   Pageable pageable) throws ValuePlusException;
