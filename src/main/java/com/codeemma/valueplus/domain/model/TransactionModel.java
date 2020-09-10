9935a181b763d37c511c275a943686fc74c69b64
John Ojetunde <john.ojetunde@telesoftas.com>
2020-09-10 09:59:19 +0300

    feat: added actual transaction status to the response

--- src/main/java/com/codeemma/valueplus/domain/model/TransactionModel.java
+++ src/main/java/com/codeemma/valueplus/domain/model/TransactionModel.java
@@ -1,6 +1,7 @@
 package com.codeemma.valueplus.domain.model;
 
 import com.codeemma.valueplus.domain.enums.TransactionStatus;
+import com.codeemma.valueplus.domain.enums.TransactionStatusFilter;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -17,6 +18,7 @@ public class TransactionModel {
     private final String currency;
     private final String reference;
     private final TransactionStatus status;
+    private final TransactionStatusFilter paystackStatus;
     private final Long userId;
     private final LocalDateTime createdAt;
     private final LocalDateTime updatedAt;
