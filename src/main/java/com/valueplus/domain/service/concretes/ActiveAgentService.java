67314185e08ec3d6b80da378f7c4339161f44890
johnojetunde <johnojetunde63@gmail.com>
2021-03-27 17:23:13 +0200

    added separate logging

--- src/main/java/com/valueplus/domain/service/concretes/ActiveAgentService.java
+++ src/main/java/com/valueplus/domain/service/concretes/ActiveAgentService.java
@@ -1,21 +1,13 @@
 package com.valueplus.domain.service.concretes;
 
 import com.valueplus.app.exception.BadRequestException;
-import com.valueplus.app.exception.ValuePlusException;
 import com.valueplus.app.model.SuperAgentFilter;
-import com.valueplus.domain.enums.OrderStatus;
 import com.valueplus.domain.model.AgentDto;
-import com.valueplus.domain.service.abstracts.ProductOrderService;
-import com.valueplus.persistence.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-
-import static com.valueplus.domain.util.FunctionUtil.emptyIfNull;
 
 @Slf4j
 @Service
@@ -23,7 +15,6 @@ import static com.valueplus.domain.util.FunctionUtil.emptyIfNull;
 public class ActiveAgentService {
 
     private final UserService userService;
-    private final ProductOrderService productOrderService;
 
     public Page<AgentDto> getAllActiveSuperAgents(SuperAgentFilter filter, Pageable pageable) {
         userService.findByReferralCode(filter.getSuperAgentCode())
@@ -31,15 +22,5 @@ public class ActiveAgentService {
 
         return userService.findAllUserBySuperAgentCode(filter.getSuperAgentCode(), filter.getStartDate(), filter.getEndDate(), pageable)
                 .map(AgentDto::valueOf);
-    }
-
-    private boolean isActiveAgent(User agent, LocalDate startDate, LocalDate endDate) {
-        try {
-            var allProducts = productOrderService.getAllProducts(null, null, OrderStatus.COMPLETED, startDate, endDate, agent);
-            return !emptyIfNull(allProducts).isEmpty();
-        } catch (ValuePlusException e) {
-            log.error("Error checking if Agent with id {} is active", agent.getId());
-            return false;
-        }
     }
 }
