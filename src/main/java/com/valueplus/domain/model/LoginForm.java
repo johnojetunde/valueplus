5bc2d311ff4e7070111916521c621b56a9f15edd
johnojetunde <johnojetunde63@gmail.com>
2021-06-29 21:38:06 +0300

    fixed the case issue with email

--- src/main/java/com/valueplus/domain/model/LoginForm.java
+++ src/main/java/com/valueplus/domain/model/LoginForm.java
@@ -2,8 +2,15 @@ package com.valueplus.domain.model;
 
 import lombok.Data;
 
+import static java.util.Optional.ofNullable;
+
 @Data
 public class LoginForm {
     private String email;
     private String password;
+
+    public String getEmail() {
+        return ofNullable(email)
+                .map(String::toLowerCase).orElse(email);
+    }
 }
--- src/main/java/com/valueplus/domain/model/PasswordReset.java
+++ src/main/java/com/valueplus/domain/model/PasswordReset.java
@@ -5,9 +5,16 @@ import lombok.Data;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
+import static java.util.Optional.ofNullable;
+
 @Data
 public class PasswordReset {
     @NotEmpty
     @Email
     private String email;
+
+    public String getEmail() {
+        return ofNullable(email)
+                .map(String::toLowerCase).orElse(email);
+    }
 }
